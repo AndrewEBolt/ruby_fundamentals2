9a8680158eda9ebadffc8c9175bc40503c2484b6
Andrew Bolt <andrewebolt@gmail.com>
2014-06-04 16:49:36 -0400

    Exercise 7.4 complete

--- exercise7.rb
+++ exercise7.rb
@@ -12,6 +12,19 @@ end
 
 display(students)
 
+#Exercise 7.2 add 43 as the number of students for cohort 4.
+students[:cohort4] = 43
+
+#Exercise 7.3 output cohort names with the keys method
+puts students.keys
+
+#Exercise 7.4 expand each class size by 5%
+#Perhaps this can be done with less code?
+students = students.map do |cohort, number| cohort, number = cohort, number * 1.05
+							cohort, number = cohort, number.to_i
+end
+
+display(students)
 
 
 
